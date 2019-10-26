c5e759c461b9c843fff861f0d605d8dc5e7dbf3b
Ivan De Jesus <ivan.dejesus@zmtsys.com>
2019-10-26 20:28:55 +0800

    [RIGELAPP-1] Initial commit, create functionalities on creating box game app logic and actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,110 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rigelGameApp';
+export class AppComponent implements OnInit {
+  rounds: number = 1;
+  boxColor: any = ['white', 'red'];
+  previousSelectedColor: string = null;
+
+  ngOnInit() { }
+
+  private getSelectedBoxAndStartTheGame(selectedColor: string): void {
+    if (this.hasReachedDeciderLevel(selectedColor)) {
+      this.checkDeciderLevelResult(selectedColor);
+      return;
+    }
+
+    switch(selectedColor) {
+      case 'white' :
+        this.handleWhiteColor();
+        break;
+
+      case 'red':
+        this.handleRedColor();
+        break;
+
+      case 'black':
+        this.handleBlackColor();
+        break;
+
+      case 'orange':
+        this.handleOrangeColor();
+        break;
+
+      case 'green':
+        this.handleGreenColor();
+        break;
+      default:
+    }
+    this.previousSelectedColor = selectedColor;
+  }
+
+  private hasReachedDeciderLevel(selectedColor: string): boolean {
+    return this.rounds > 3 && this.previousSelectedColor !== 'black' && (selectedColor == 'green' || selectedColor == 'orange');
+  }
+
+  private checkDeciderLevelResult(selectedColor: string): void {
+    if (this.previousSelectedColor !== selectedColor) {
+      this.gameOverAndReset();
+    } else if (this.previousSelectedColor == selectedColor) {
+      this.winTheGameAndReset();
+    }
+  }
+
+  private gameOverAndReset(): void {
+    alert('Game Over');
+    this.boxColor = ['white', 'red'];
+    this.rounds = 1;
+  }
+
+  private winTheGameAndReset(): void {
+    alert('You Win');
+    this.boxColor = ['white', 'red'];
+    this.rounds = 1;
+  }
+
+  private handleWhiteColor(): void {
+    if (this.rounds == 1) {
+      this.boxColor = ['white', 'orange'];
+      this.rounds++;
+    } else if (this.rounds == 2) {
+      this.boxColor = ['black', 'red'];
+      this.rounds++;
+    }
+  }
+
+  private handleRedColor(): void {
+    if (this.rounds == 1) {
+      this.boxColor = ['black', 'red'];
+      this.rounds++;
+    } else {
+      this.gameOverAndReset();
+    }
+  }
+
+  private handleBlackColor(): void {
+    this.boxColor = ['green', 'orange', 'black'];
+    this.rounds++;
+  }
+
+  private handleOrangeColor(): void {
+    if (this.rounds == 2) {
+      this.boxColor = ['green', 'orange', 'black'];
+      this.rounds++;
+    } else if (this.rounds >= 3) {
+      this.boxColor = ['green', 'orange'];
+      this.rounds++;
+    }
+  }
+
+  private handleGreenColor(): void {
+    if (this.rounds >= 3) {
+      this.boxColor = ['green', 'orange'];
+      this.rounds++;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
